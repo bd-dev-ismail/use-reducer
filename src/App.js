35fb8464b2f886d4dd5e3c9b14fc77de213775bc
Ap-Reborn <ashrafulamin654@gmail.com>
2023-03-24 07:51:15 +0600

    simple counter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import Counter from "./pages/Counter";
 import LongForm from "./pages/LongForm";
+import ShortForm from "./pages/ShortForm";
 
 function App() {
-  return <LongForm />;
+  // return <LongForm />;
+  // return <ShortForm/>;
+  return <Counter/>
 }
 
 export default App;
---
+++ src/pages/Counter.js
@@ -0,0 +1,46 @@
+import React, { useReducer } from "react";
+// import { initialState, reducer } from '../state/formReducer';
+
+const Counter = () => {
+  //inital state declare
+  const initalState = 0;
+  //write reducer function logics
+  const reducer = (state, action) => {
+    console.log(action);
+    if (action.type === "INCREMENT") {
+      return state + action.payload.count;
+    } else if (action.type === "DECREMENT") {
+      return state - action.payload.count;
+    }
+  };
+  //how to call useReducer hook
+  const [state, dispatch] = useReducer(reducer, initalState);
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <div>
+        <button
+          className=" px-4 py-3 bg-blue-500 rounded-md font-semibold text-white text-lg disabled:bg-gray-500"
+          onClick={() =>
+            dispatch({
+              type: "DECREMENT",
+              payload: {count: 5},
+            })
+          }
+        >
+          Decreiment --
+        </button>
+        <p className="text-5xl text-center font-bold text-purple-500">
+          {state}
+        </p>
+        <button
+          className=" px-4 py-3 bg-indigo-500 rounded-md font-semibold text-white text-lg disabled:bg-gray-500"
+          onClick={() => dispatch({ type: "INCREMENT", payload: {count: 5} })}
+        >
+          Increment ++
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
